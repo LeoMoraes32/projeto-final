3d8f3c5e045973582481649782fb5e496d73f689
Leonardo Moraes <leonardo32moraes@hotmail.com>
2021-11-25 23:22:03 -0300

    Creating validation with joi for reserve

--- src/app/validation/reserve/create.js
+++ src/app/validation/reserve/create.js
@@ -1,5 +1,25 @@
 const Joi = require('joi');
+const { id } = require('../../utils/regex');
 
 module.exports = async (req, res, next) => {
-    
-}
+  try {
+    const schema = Joi.object({
+      id_user: Joi.string().required().regex(id).min(24).max(24),
+      data_inicio: Joi.string().required(),
+      data_fim: Joi.string().required(),
+      id_carro: Joi.string().required().regex(id),
+      id_locadora: Joi.string().required().regex(id),
+      valor_final: Joi.number().required()
+    });
+    const { error } = await schema.validate(req.body, { abortEarly: false });
+    if (error) throw error;
+    return next();
+  } catch (error) {
+    return res.status(400).json(
+      error.details.map((detail) => ({
+        description: detail.message,
+        name: detail.path.join('.')
+      }))
+    );
+  }
+};
